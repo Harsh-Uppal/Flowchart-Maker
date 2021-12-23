92ef70f89653ac816f02d4e39ff6be1a21167304
unknown <harshuppal14122008@gmail.com>
2021-12-23 12:01:59 +0530

    Programmed loadProperties function

--- Scripts/inspector.js
+++ Scripts/inspector.js
@@ -3,11 +3,11 @@ class Inspector {
         if (this.node == null)
             this.node = document.querySelector('.inspector');
 
-        this.node.style.marginTop = val ? '0' : '100%';
-        this.active = val;
-
         if(val)
             this.loadProperties();
+
+        this.node.style.marginTop = val ? '0' : '100%';
+        this.active = val;
     }
     static setInspectorProperties(props) {
         this.inspectingProperties = props;
@@ -17,13 +17,23 @@ class Inspector {
             return;
         
         let propertyObjContainer = document.querySelector('.inspector > .properties');
-        console.log(propertyObjContainer);
         for(let property in this.inspectingProperties){
+            let newPropertyContainer = document.createElement('div');
+            let propertyLabel = document.createElement('label');
+            let input = document.createElement('input');
 
+            propertyLabel.textContent = property.name;
+            input.type = property.type;
+            input.value = property.val;
+
+            newPropertyContainer.appendChild(propertyLabel);
+            newPropertyContainer.appendChild(input);
+
+            propertyObjContainer.appendChild(newPropertyContainer);
         }
     }
 }
 
-function createProperty(type, value) {
-    return { type: type, val: value };
+function createProperty(name, type, value) {
+    return { name:name, type: type, val: value };
 }
